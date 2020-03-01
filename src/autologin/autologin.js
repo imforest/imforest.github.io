d666036c06e22ef74fd1a5684bb978cb00cb46a6
kkb <imforest@gmail.com>
2020-03-01 14:06:42 +0900

    Update chrome extensions
    - 특정 URL에서 자동로그인하는 js 추가
    - 백그라운드 js 추가

--- src/autologin/autologin.js
+++ src/autologin/autologin.js
@@ -4,6 +4,5 @@ var searchUrl = "www.msde.co.kr:18080/kriso/main/userLogin.do";
 if (url.indexOf(searchUrl) >= 0) {
     document.getElementById("loginid").value = "kibeom";
     document.getElementById("password").value = "1111";
-    //document.getElementById("btnLogin").click();
+    document.getElementById("btnLogin").click();
 }
-
